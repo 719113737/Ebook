c6769f644b17af7aeb6fa93a2b3185b117c4e2c5
tsluqn <t-l-q@163.com>
2021-04-10 12:07:09 +0800

    add some htmls

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Book;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class MainController {
@@ -17,8 +21,31 @@ public class MainController {
      * @return 主界面 index.html
      */
     @RequestMapping("/")
-    public String main_page() {
+    public String main_page(Model model) {
+        List<Book> bookList = new ArrayList<>();
+        for (int i=0;i<3;i++){
+            Book book = new Book();
+            book.id = "1";
+            book.title = "算法导论";
+            book.mAbstract = "《算法导论》自第一版出版以来，已经成为世界范围内广泛使用的大学教材和专业人员的标准参考手册。本书全面论述了算法的内容，从一定深度上涵盖了算法的诸多方面，同时其讲授和分析方法又兼顾了各个层次读者的接受能力...";
+            book.imagePath = "/img/book1.jpg";
+            book.category = "IT/算法";
+            book.filePath = "";
+            book.publishDate = "";
+            book.phases = "";
+            bookList.add(book);
+        }
+        model.addAttribute("bookList", bookList);
         return "index";
+    }
+
+    /**
+     * 图书简介
+     * @return 图书简介页
+     */
+    @RequestMapping("/abstract")
+    public String mAbstract(){
+        return "abstract";
     }
 
     /**
@@ -41,6 +68,14 @@ public class MainController {
     }
 
     /**
+     * 登录
+     * @return 登录页面
+     */
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+    /**
      * 成功登录跳转界面
      * @return 成功跳转界面 login_success.html
      */
@@ -54,7 +89,7 @@ public class MainController {
      * @return 个人空间界面 personal.html
      */
     @RequestMapping("/personal")
-    public String peronal_page() {
+    public String personal_page() {
         return "personal";
     }
 
